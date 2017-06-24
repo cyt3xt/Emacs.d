2d62cb4e5d0092172b5bbc7325c46f240810a958
Cyt XT3 <cyt3xt@me.com>
2017-06-25 00:15:59 +0800

    UPDATE

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;;; Version Requirement
 (when (version<= emacs-version "24")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
@@ -6,10 +5,22 @@
 
 ;;; Set up package
 (require 'package)
+;;
+;; (add-to-list 'package-archives
+;;              '("gnu" . " https://elpa.gnu.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;; For 共国
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("gnu" . "https://elpa.emacs-china.org/gnu/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://elpa.emacs-china.org/melpa/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://elpa.emacs-china.org/melpa-stable/") t)
+
 (package-initialize)
 
 
@@ -22,19 +33,11 @@
 (eval-when-compile
   (require 'use-package))
 
+(setq use-package-always-ensure t)
 (setq use-package-verbose t)
 
 
-;;; Basic config
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(tooltip-mode -1)
-
-(setq inhibit-startup-message t)
-(setq initial-scratch-message "")
-
 (use-package auto-compile
-  :ensure t
   :config (auto-compile-on-load-mode))
 (setq load-prefer-newer t)
 
@@ -43,11 +46,18 @@
 
 
 ;;; UI
+;; Basic config
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
 ;; Terminal
 (unless window-system
   (defconst *is-a-terminal* t)
   (message "It is a Terminal")
-  (menu-bar-mode -1))
+  (menu-bar-mode -1)
+  (org-babel-load-file (concat user-emacs-directory "config.org")))
 ;; Window-System
 (when window-system 
   (if (eq system-type 'darwin)
@@ -58,7 +68,4 @@
     ;; Linux
     (progn
       (defconst *is-a-linux* t)))
-  (org-babel-load-file (concat user-emacs-directory "config.org")))
-
-
-
+  (org-babel-load-file (concat user-emacs-directory "config.org")))  
